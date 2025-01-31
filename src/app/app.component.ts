df50e220e290de21b3bc60683aaecee73f2fe020
rambaudAymeric <rambaudaymeric@gmail.com>
2025-01-31 12:26:32 +0100

    scss pour la calculatrice ok encore des petits truc à aire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./menu/header/header.component";
 import { CalculatriceComponent } from "./body/calculatrice/calculatrice.component";
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, CalculatriceComponent],
+  imports: [HeaderComponent, CalculatriceComponent, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
