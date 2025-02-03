2bc1b45125233e94b2c816b66f7a7f87eaf660a0
rambaudAymeric <rambaudaymeric@gmail.com>
2025-02-03 18:32:07 +0100

    fonctionnalité du calcule avec un reged en cours

--- src/app/body/calculatrice/calculatrice.component.ts
+++ src/app/body/calculatrice/calculatrice.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ScreenCalculatorComponent } from '../screen-calculator/screen-calculator.component';
-import { KeyCalculatorComponent } from "../key-calculator/key-calculator.component";
 
 @Component({
   selector: 'app-calculatrice',
-  imports: [ScreenCalculatorComponent, KeyCalculatorComponent],
+  imports: [ScreenCalculatorComponent],
   templateUrl: './calculatrice.component.html',
   styleUrl: './calculatrice.component.scss'
 })
@@ -12,9 +11,42 @@ import { KeyCalculatorComponent } from "../key-calculator/key-calculator.compone
 export class CalculatriceComponent implements OnInit {
 
   tilte!: string;
-  items = Array.from({ length: 9 }, (_, i) => i);
+  keypadTab = Array.from({ length: 9 }, (_,i) => i);
+
+  valueSymbol!: string;
+
+  tabNumber: string [] = [];
 
   ngOnInit(): void {
     this.tilte = "Calcultratice"
+    this.tabNumber = [];
+  }
+
+  onNumberClick(value: number) {
+    this.tabNumber = [...this.tabNumber, value.toString()];
+  }
+
+  addSymbol(symbol: string) {
+    if (!this.tabNumber[this.tabNumber.length-1].match(/[X+\-/%]/)) {
+      this.tabNumber = [...this.tabNumber, symbol];
+    } else {
+      this.tabNumber.pop();
+      this.tabNumber = [...this.tabNumber, symbol];
+    }
+  }
+
+  resultaCalcule() {
+    // let input = this.tabNumber.join('')
+    let input = "475X12"
+    let result = input.match('/(\d+)([X+\-/%])(\d+)/');
+    if (result) {
+      console.log("tata", result[1]);
+    } else {
+      console.log("toto", result);  
+    }
+  }
+
+  onDelete() {
+    this.tabNumber = [];
   }
 }
